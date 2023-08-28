5b662407d531eead915f58f7d17f1b0e720b73ed
Tomas Emr <tommy.emr@gmail.com>
2023-08-28 15:52:43 +0200

    CreateNewTaskDTO request for new todo task

---
+++ src/main/java/com/example/simple_todo_app/models/dtos/CreateNewTaskDTO.java
@@ -0,0 +1,15 @@
+package com.example.simple_todo_app.models.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateNewTaskDTO {
+
+    private String name;
+    private String description;
+
+}
