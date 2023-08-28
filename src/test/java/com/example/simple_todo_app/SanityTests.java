97f36362d8139924ea98a4217415e7a1de255c17
Tomas Emr <tommy.emr@gmail.com>
2023-08-28 13:36:36 +0200

    Sanity check test

---
+++ src/test/java/com/example/simple_todo_app/SanityTests.java
@@ -0,0 +1,15 @@
+package com.example.simple_todo_app;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SanityTests {
+
+    @Test
+    void alwaysPasses() {
+        assertTrue(true);
+    }
+
+
+}
