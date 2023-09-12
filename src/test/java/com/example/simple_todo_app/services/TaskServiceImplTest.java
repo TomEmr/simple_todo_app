27ee5cdcfe96c330d81d601b673f210dcc32573a
Tomas Emr <tommy.emr@gmail.com>
2023-09-12 23:14:09 +0200

    service unit tests fixed for security

--- src/test/java/com/example/simple_todo_app/services/TaskServiceImplTest.java
+++ src/test/java/com/example/simple_todo_app/services/TaskServiceImplTest.java
@@ -4,14 +4,19 @@ import com.example.simple_todo_app.exceptions.MissingDataException;
 import com.example.simple_todo_app.exceptions.NotFoundException;
 import com.example.simple_todo_app.exceptions.OverExtendedLengthException;
 import com.example.simple_todo_app.models.Task;
+import com.example.simple_todo_app.models.User;
 import com.example.simple_todo_app.models.dtos.CreateNewTaskDTO;
 import com.example.simple_todo_app.repositories.TaskRepository;
+import com.example.simple_todo_app.repositories.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.util.List;
 import java.util.Optional;
@@ -27,11 +32,31 @@ class TaskServiceImplTest {
     @Mock
     private TaskRepository taskRepository;
 
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private Authentication authentication;
+
+    @Mock
+    private SecurityContext securityContext;
+
+    private User user;
+
     @BeforeEach
     void setUp() {
         MockitoAnnotations.openMocks(this);
 
-        when(taskRepository.findAll()).thenReturn(List.of(
+        user = User
+                .builder()
+                .id(1L)
+                .email("test@test.com")
+                .password("password")
+                .build();
+
+        when(userRepository.findByEmail(anyString())).thenReturn(Optional.of(user));
+
+        when(taskRepository.findAllByUser(user)).thenReturn(List.of(
                 Task.builder().id(1L).title("Task 1").completed(false).build(),
                 Task.builder().id(2L).title("Task 2").completed(true).build(),
                 Task.builder().id(3L).title("Task 3").completed(false).build(),
@@ -39,16 +64,21 @@ class TaskServiceImplTest {
                 Task.builder().id(5L).title("Task 5").completed(false).build()
         ));
 
-        when(taskRepository.findAllByCompletedTrue()).thenReturn(List.of(
+        when(taskRepository.findAllByUserAndCompletedTrue(user)).thenReturn(List.of(
                 Task.builder().id(2L).title("Task 2").completed(true).build(),
                 Task.builder().id(4L).title("Task 4").completed(true).build()
         ));
 
-        when(taskRepository.findAllByCompletedFalse()).thenReturn(List.of(
+        when(taskRepository.findAllByUserAndCompletedFalse(user)).thenReturn(List.of(
                 Task.builder().id(1L).title("Task 1").completed(false).build(),
                 Task.builder().id(3L).title("Task 3").completed(false).build(),
                 Task.builder().id(5L).title("Task 5").completed(false).build()
         ));
+
+        when(securityContext.getAuthentication()).thenReturn(authentication);
+        when(authentication.getName()).thenReturn("test@test.com");
+
+        SecurityContextHolder.setContext(securityContext);
 
     }
 
@@ -75,13 +105,13 @@ class TaskServiceImplTest {
     @DisplayName("Delete all completed tasks")
     void deleteAllCompletedTasks() {
         taskService.deleteAllCompletedTasks();
-        verify(taskRepository, times(1)).deleteAllByCompletedTrue();
+        verify(taskRepository, times(1)).deleteAllByUserAndCompletedTrue(user);
     }
 
     @Test
     @DisplayName("Update task title by id")
     void updateTitleById() {
-        Task task = Task.builder().id(1L).title("Task 1").completed(false).build();
+        Task task = Task.builder().id(1L).title("Task 1").user(user).completed(false).build();
         when(taskRepository.findById(1L)).thenReturn(Optional.of(task));
         when(taskRepository.save(task)).thenReturn(task);
         assertEquals("Task 2", taskService.updateTitleById(1L, "Task 2").getTitle());
@@ -99,7 +129,7 @@ class TaskServiceImplTest {
     @Test
     @DisplayName("Change task to completed by id")
     void updateCompletedById() {
-        Task task = Task.builder().id(1L).title("Task 1").completed(false).build();
+        Task task = Task.builder().id(1L).title("Task 1").user(user).completed(false).build();
         when(taskRepository.findById(1L)).thenReturn(Optional.of(task));
         when(taskRepository.save(task)).thenReturn(task);
         assertEquals(true, taskService.updateCompletedById(1L).getCompleted());
