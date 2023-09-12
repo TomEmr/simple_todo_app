5290c82a24810ff60f7d6efcf814411bd00a71c0
Tomas Emr <tommy.emr@gmail.com>
2023-09-12 22:49:20 +0200

    JwtServiceImpl

---
+++ src/main/java/com/example/simple_todo_app/services/JwtServiceImpl.java
@@ -0,0 +1,75 @@
+package com.example.simple_todo_app.services;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+@Service
+public class JwtServiceImpl {
+
+    @Value("${SECRET_KEY}")
+    private String SECRET_KEY;
+    public String extractUserEmailFromAccessToken(String jwtToken) {
+        return extractAccessTokenClaim(jwtToken, Claims::getSubject);
+    }
+
+    public <T> T extractAccessTokenClaim(String token, Function<Claims, T> claimsResolver) {
+        Claims claims = extractAllAccessTokenClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    public String generateAccessToken(UserDetails userDetails) {
+        Map<String, Object> extraClaims = new HashMap<>();
+        extraClaims.put("role", userDetails.getAuthorities());
+        return generateAccessToken(extraClaims, userDetails);
+    }
+
+    public String generateAccessToken(Map<String, Object> extraClaims, UserDetails userDetails) {
+        return Jwts
+                .builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String userEmail = extractUserEmailFromAccessToken(token);
+        return (userEmail.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractAccessTokenClaim(token, Claims::getExpiration);
+    }
+
+    private Claims extractAllAccessTokenClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
